cbc7515505f5daa21306a365439b0af9c74c7348
Peter <Peter@example.com>
2020-08-03 13:42:50 -0400

    confirm working

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          LOL cont integeration
+          confirm working
         </a>
       </header>
     </div>
