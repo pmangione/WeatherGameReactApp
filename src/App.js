7fe71c287834f532320ee2a9b1432e7b53307338
Peter <Peter@example.com>
2020-08-04 20:46:23 -0400

    confirm workin aug 4 2020

--- src/App.js
+++ src/App.js
@@ -3,25 +3,12 @@ import React, { useState } from 'react';
 
 import logo from './logo.svg';
 import './App.css';
+import { createPortal } from 'react-dom';
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          confirm working August 4th 1110PM Work?
-        </a>
-      </header>
-    </div>
+    <button onClick={() => setCounter(counter + 1)}>{counter}</button>
   );
 }
 
